edbf4a8b5aff8463556f44aa590b302976f13deb
azaxm tukam <azaxsql@gmail.com>
2026-05-01 13:34:02 +0700

    Update build.gradle.kts

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -29,8 +29,7 @@ dependencies {
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.encoding)
     implementation(libs.brotli)
-    implementation("com.github.Z-MusicGroup:Z-MusicExtractor:6305155") {
-        exclude(group = "com.google.protobuf")
+    implementation("com.github.MetrolistGroup:MetrolistExtractor:6305155") {        exclude(group = "com.google.protobuf")
     }
     implementation(libs.timber)
     testImplementation(libs.junit)
